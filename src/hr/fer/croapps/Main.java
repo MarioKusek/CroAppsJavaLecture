b9b3a8b3062dd9719d2ad2c963e9b396b45fdb4e
mkusek <mario.kusek@fer.hr>
2015-05-12 09:26:34 +0200

    Two instances

--- src/hr/fer/croapps/Main.java
+++ src/hr/fer/croapps/Main.java
@@ -2,12 +2,19 @@ package hr.fer.croapps;
 
 public class Main {
     public static void main(String[] args) {
-        ShortAnswerQuestion question = new ShortAnswerQuestion(
+        ShortAnswerQuestion question1 = new ShortAnswerQuestion(
                 "What is the Android programming language?", "Java");
+        ShortAnswerQuestion question2 = new ShortAnswerQuestion(
+                "What is the iPhone programming language?", "Objective-C");
 
-        System.out.println(question.getText());
+        System.out.println(question1.getText());
+        System.out.print("objective-c: " +
+                question1.isCorrectAnswer("objective-c"));
+        System.out.println(" java: " + question1.isCorrectAnswer("java"));
 
-        System.out.println("CC: " + question.isCorrectAnswer("C"));
-        System.out.println("java: " + question.isCorrectAnswer("java"));
+        System.out.println(question2.getText());
+        System.out.print("objective-c: " +
+                question2.isCorrectAnswer("objective-c"));
+        System.out.println(" java: " + question2.isCorrectAnswer("java"));
     }
 }
